5adc1a76aca29c2deeb198a2397d49d65f26deeb
reshux <duruhanunsal@gmail.com>
2019-04-16 20:21:43 +0000

    Exercise 9 done

--- lib/store.rb
+++ lib/store.rb
@@ -3,11 +3,21 @@ class Store < ActiveRecord::Base
     validates :name, length: {minimum: 3}
     validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
     validate :carries_apparel
+    before_destroy :check_employees, prepend:true
 
     def carries_apparel
         if !mens_apparel && !womens_apparel
             errors.add(:mens_apparel, "is not carried.")
             errors.add(:womens_apparel, "is not carried either.")
+        end
+    end
+
+    private
+
+    def check_employees
+        if employees.size > 0
+            errors[:base] << "cannot be deleted."
+            throw :abort
         end
     end
 end
