70a8760cb93049f40309e6dcdd2d4aa2559b19f9
reshux <duruhanunsal@gmail.com>
2019-04-16 19:17:54 +0000

    Exercise 8 done

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,16 @@ class Employee < ActiveRecord::Base
     validates :hourly_rate,
         numericality: {greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
     validates :store_id, presence: true
+
+    after_create :create_password
+    private
+
+    def create_password
+        generator = ""
+        8.times do 
+            generator += [*'0'..'9', *'a'..'z', *'A'..'Z'].sample
+        end
+        self.password = generator
+        self.save
+      end
 end
