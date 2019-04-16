57a1991dd36e477d73b43f2865a08219ba24f528
reshux <duruhanunsal@gmail.com>
2019-04-16 16:05:31 +0000

    counter added

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -13,5 +13,6 @@ puts "----------"
 Store.create!(name:"Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create!(name:"Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create!(name:"Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-
-binding.pry
+puts "----------"
+puts "Number of stores added to database"
+puts Store.all.count
